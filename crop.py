76c241e0fb6655be64a1915c1b7383f95652c918
Andrea <andreafmguillen-cloud@users.noreply.github.com>
2026-03-20 02:10:47 +0100

    Corregir corte superior del logo y arreglar fondo del footer

--- crop.py
+++ crop.py
@@ -1,6 +1,7 @@
 from PIL import Image
 
 img = Image.open('logo.png')
-box = (100, 100, 924, 650)
+# Crop only the bottom part where the text is, leave the sides and top alone
+box = (0, 0, 1024, 580)
 cropped = img.crop(box)
 cropped.save('logo_flower.png')
